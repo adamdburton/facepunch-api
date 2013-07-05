c3e148b0cbdbb43d828287dc000c0439af54bb69
Adam Burton <adam@burt0n.net>
2013-07-05 10:06:08 +0100

    Added pushover error logging

--- index.php
+++ index.php
@@ -1,10 +1,39 @@
 <?php
 
+include('../burt0n.net/fpbots/functions/pushover.php');
+
+function pushover_errors($code, $message, $file, $line)
+{
+	switch($code)
+	{
+		case 1:     $e_type = 'E_ERROR'; break;
+		case 2:     $e_type = 'E_WARNING'; break;
+		case 4:     $e_type = 'E_PARSE'; break;
+		case 8:     $e_type = 'E_NOTICE'; break;
+		case 16:    $e_type = 'E_CORE_ERROR'; break;
+		case 32:    $e_type = 'E_CORE_WARNING'; break;
+		case 64:    $e_type = 'E_COMPILE_ERROR'; break;
+		case 128:   $e_type = 'E_COMPILE_WARNING'; break;
+		case 256:   $e_type = 'E_USER_ERROR'; break;
+		case 512:   $e_type = 'E_USER_WARNING'; break;
+		case 1024:  $e_type = 'E_USER_NOTICE'; break;
+		case 2048:  $e_type = 'E_STRICT'; break;
+		case 4096:  $e_type = 'E_RECOVERABLE_ERROR'; break;
+		case 8192:  $e_type = 'E_DEPRECATED'; break;
+		case 16384: $e_type = 'E_USER_DEPRECATED'; break;
+		case 30719: $e_type = 'E_ALL'; break;
+		default:    $e_type = 'E_UNKNOWN'; break;
+	}
+	
+	pushover(sprintf('%s: "%s" (%s line %s)', $e_type, $message, $file, $line));
+}
+
 define('DEBUG', true);
 
 if(DEBUG)
 {
-	ini_set('display_errors', 1);
+	//ini_set('display_errors', 1);
+	set_error_handler('pushover_errors', E_ALL ^ E_NOTICE);
 }
 
 $start_time = microtime(true);
