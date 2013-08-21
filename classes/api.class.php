ff2845377fbde98294c509397c1c5fb6e20b8570
Adam Burton <adam@burt0n.net>
2013-08-21 11:49:25 +0100

    Change to fixed function

--- classes/api.class.php
+++ classes/api.class.php
@@ -466,7 +466,6 @@ function request($url, $fields = array(), $method = 'GET', $headers = array(), $
 	$c = curl_init($url);
 	
 	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
 	
 	if($returnheaders)
 	{
@@ -495,7 +494,7 @@ function request($url, $fields = array(), $method = 'GET', $headers = array(), $
 		curl_setopt($c, CURLOPT_HTTPHEADER, headers_to_curl_headers($headers));
 	}
 	
-	$out = curl_exec($c);
+	$out = curl_exec_follow($c);
 	
 	curl_close($c);
 	
@@ -512,6 +511,78 @@ function headers_to_curl_headers($array)
 	}
 	
 	return $out;
+}
+
+// From http://php.net/manual/en/function.curl-setopt.php#102121
+
+function curl_exec_follow($ch, &$maxredirect = null)
+{
+	$mr = $maxredirect === null ? 5 : intval($maxredirect);
+	
+	if(ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off'))
+	{
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, $mr);
+	}
+	else
+	{
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+		if($mr > 0)
+		{
+			$newurl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+			
+			$rch = curl_copy_handle($ch);
+			curl_setopt($rch, CURLOPT_HEADER, true);
+			curl_setopt($rch, CURLOPT_NOBODY, true);
+			curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
+			curl_setopt($rch, CURLOPT_RETURNTRANSFER, true);
+			
+			do {
+				curl_setopt($rch, CURLOPT_URL, $newurl);
+				
+				$header = curl_exec($rch);
+				
+				if(curl_errno($rch))
+				{
+					$code = 0;
+				}
+				else
+				{
+					$code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
+					
+					if($code == 301 || $code == 302)
+					{
+						preg_match('/Location:(.*?)\n/', $header, $matches);
+						$newurl = trim(array_pop($matches));
+					}
+					else
+					{
+						$code = 0;
+					}
+				}
+			} while($code && --$mr);
+			
+			curl_close($rch);
+			
+			if(!$mr)
+			{
+				if($maxredirect === null)
+				{
+					trigger_error('Too many redirects. When following redirects, libcurl hit the maximum amount.', E_USER_WARNING);
+				}
+				else
+				{
+					$maxredirect = 0;
+				}
+				
+				return false;
+			}
+			
+			curl_setopt($ch, CURLOPT_URL, $newurl);
+		}
+	}
+	
+	return curl_exec($ch); 
 }
 
 function quick_match($for, $in)
